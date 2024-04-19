f22eb954d5057991e39bb5e6c37982dd97f25532
gn.liu <609141551@qq.com>
2024-04-19 15:16:53 +0800

    新增启动脚本

---
+++ startupserver.bat
@@ -0,0 +1,6 @@
+
+
+start java -Xmx512m -Dfile.encoding=UTF-8 -jar  D:\moguapp\server\mogu_gateway-0.0.1-SNAPSHOT.jar
+rem start java -Xmx512m -Dfile.encoding=UTF-8 -jar  D:\moguapp\server\mogu_picture-0.0.1-SNAPSHOT.jar
+rem start java -Xmx512m -Dfile.encoding=UTF-8 -jar  D:\moguapp\server\mogu_web-0.0.1-SNAPSHOT.jar
+rem start java -Xmx512m -Dfile.encoding=UTF-8 -jar  D:\moguapp\server\mogu_admin-0.0.1-SNAPSHOT.jar
---
+++ startuptools.bat
@@ -0,0 +1,10 @@
+
+rem  nacos
+rem start D:\tools\nacos-server-2.3.1-SNAPSHOT\nacos\bin\startup.cmd -m standalone
+
+rem redis
+start D:\software\Redis-x64-5.0.14.1\redis-server.exe
+
+rem zipkin
+start java -jar D:\tools\zipkin-server-2.12.9-exec.jar
+
